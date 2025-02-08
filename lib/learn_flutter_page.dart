edb4f23c66951391a36775581297eeaac1b98a86
YamiProgrammer <ticmanclarence@gmail.com>
2025-02-08 21:32:55 +0800

    SingleChildScrollView

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -23,86 +23,91 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           icon: Icon(Icons.arrow_back_ios),
         ),
       ),
-      body: Column(
-        children: [
-          Image.asset(
-            'images/clarence.jpg',
-            height: 300,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.black,
-          ),
-          Container(
-            margin: EdgeInsets.all(10.0),
-            padding: EdgeInsets.all(10.0),
-            color: Colors.black87,
-            width: double.infinity,
-            child: Center(
-              child: Text(
-                'Hi! Im Clarence Ticman',
-                style: TextStyle(
-                  color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              'images/clarence.jpg',
+              height: 300,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              margin: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(10.0),
+              color: Colors.black87,
+              width: double.infinity,
+              child: Center(
+                child: Text(
+                  'Hi! Im Clarence Ticman',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: isSwitch ? Colors.green : Colors.blue,
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isSwitch ? Colors.green : Colors.blue,
               ),
-            onPressed: () {
-              debugPrint('Elevated Button');
-            },
-            child: Text('Elevated Button'),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint('Outlined Button');
-            },
-            child: Text('Outlined Button'),
-          ),
-          TextButton(
-            onPressed: () {
-              debugPrint('Text Button');
-            },
-            child: Text('Text Button'),
-          ),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              debugPrint('This is the row');
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.red,
-                ),
-                Text('Row Widget'),
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.red,
-                ),
-              ],
+              onPressed: () {
+                debugPrint('Elevated Button');
+              },
+              child: Text('Elevated Button'),
             ),
-          ),
-          Switch(
-              value: isSwitch,
-              onChanged: (bool newBool) {
-                setState(() {
-                  isSwitch = newBool;
-                });
-              }),
-              Checkbox(value: isCheckbox, onChanged: (bool? newBool){
-                setState(() {
-                  isCheckbox = newBool;
-                });
-              },),
-        ],
+            OutlinedButton(
+              onPressed: () {
+                debugPrint('Outlined Button');
+              },
+              child: Text('Outlined Button'),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint('Text Button');
+              },
+              child: Text('Text Button'),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint('This is the row');
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.red,
+                  ),
+                  Text('Row Widget'),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+                value: isSwitch,
+                onChanged: (bool newBool) {
+                  setState(() {
+                    isSwitch = newBool;
+                  });
+                }),
+            Checkbox(
+                value: isCheckbox,
+                onChanged: (bool? newBool) {
+                  setState(() {
+                    isCheckbox = newBool;
+                  });
+                }),
+                Image.network('https://blubbyweb.com/wp-content/uploads/2024/02/10000543394622246869468213112.jpg?w=1200')
+          ],
+        ),
       ),
     );
   }
