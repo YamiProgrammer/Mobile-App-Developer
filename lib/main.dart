786ff213bbcf26788d2b79d6183cc70a9f6287a5
YamiProgrammer <ticmanclarence@gmail.com>
2025-02-08 21:09:01 +0800

    GestureDetector Widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.deepPurple,
         useMaterial3: false,
       ),
       home: const RootPage(),
